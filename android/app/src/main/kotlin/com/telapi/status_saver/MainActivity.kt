5be0215775bb0b4b5cb5abde2d163f69b056b9a5
thisisazeez <thisisazeez0@gmail.com>
2023-03-18 06:09:24 +0000

    fixed

--- android/app/src/main/kotlin/com/telapi/status_saver/MainActivity.kt
+++ android/app/src/main/kotlin/com/telapi/status_saver/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.telapi.status_saver
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 
-class MainActivity: FlutterActivity() {
+public class MainActivity extends FlutterActivity {
 }
